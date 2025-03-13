c54af15e70816cc9b4f4b7071e010f687f1548eb
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-03-13 11:32:12 +0000

    Проверка ДЗ

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
 import utils
 
+# TODO: Аннотации ко всем идентификаторам в коде
 
-# Закомментируйте чтобы протестировать работу программы
-# import pyperclip
-
+flag: bool = True
+try:
+    import pyperclip
+except ModuleNotFoundError:
+    flag = False
 
 
 def main():
-
     lang: str = utils.choice_lan()
 
-
-    if lang == "ru":
-        print(f"{utils.lang_dict["enter_your_message_ru"]} 'leet': ")
-    if lang == "en":
-        print(f"{utils.lang_dict["enter_your_message_en"]} 'leet': ")
+    print(utils.lang_dict.get(lang).get("enter_your_message") + " 'leet'")
     leet = input("> ")
+
     print()
     leetspeak = utils.english_to_leetspeak(leet)
     print(leetspeak)
 
-
     utils.write_result(leetspeak)
 
-
-    # Закомментируйте следующую строку кода, чтобы
-    # протестировать работу программы
-    # pyperclip.copy(leetspeak)
-    # print("[INFO] Результат скопирован в буфер обмена.")
+    if flag:
+        pyperclip.copy(leetspeak)
+        print("[INFO] Результат скопирован в буфер обмена.")
 
 
 if __name__ == "__main__":
